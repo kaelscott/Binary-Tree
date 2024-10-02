a4010e4aa3c12b1c73238a9aa5e8063758c1db43
kael <kaelscott8@gmail.com>
2024-10-02 10:58:39 -0300

    binary tree atributes

---
+++ BinaryTree.java
@@ -0,0 +1,14 @@
+public class BinaryTree {
+    private Node root;
+    private Node current = root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void insert(String morseCode, String character) {
+
+        for(int i = 0;i<)
+    }
+}
+
---
+++ Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    public String data;
+    public Node left;
+    public Node right;
+
+    public Node(String data) {
+        this.data = String.valueOf(data);
+        this.left = null;
+        this.right = null;
+    }
+
+
+}
